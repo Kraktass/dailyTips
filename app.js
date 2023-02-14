8f7769469e60154b6cedd4fa6c65de4c77c404cf
Jakob Sjöholm <jakob.sjoholm@live.se>
2023-02-14 13:34:24 +0100

    cssfiles

--- app.js
+++ app.js
@@ -36,13 +36,20 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var urlRandom = 'https://zenquotes.io/api/random/116b01ed99f7230a36d036a31adcc0f796ed6a70';
 var urlDaily = 'https://zenquotes.io/api/today/116b01ed99f7230a36d036a31adcc0f796ed6a70';
+var authorUrl = 'https://zenquotes.io/api/authors/116b01ed99f7230a36d036a31adcc0f796ed6a70';
+var userKey = '116b01ed99f7230a36d036a31adcc0f796ed6a70';
 var randomButton = document.querySelector('.randomize');
 var saveButton = document.querySelector('.saveButton');
+var saveDaily = document.querySelector('.saveDaily');
 var dailyDiv = document.querySelector('.dailyTip');
 var randomDiv = document.querySelector('.randomTip');
 var buttonDiv = document.querySelector('.buttons');
 var savedQuotes = document.querySelector('.savedQuotes');
 var savedDiv = document.querySelector('#savedDiv');
+var authorList = document.querySelector('.authorList');
+var body = document.querySelector('#documentFrame');
+var quotes = document.querySelector('.quotes');
+var authorCard = document.querySelector('.authorCard');
 //--------------load daily tip with onload event--------------
 function getDailyTip() {
     return __awaiter(this, void 0, void 0, function () {
@@ -61,6 +68,12 @@ function getDailyTip() {
     });
 }
 ;
+saveDaily.addEventListener('click', function () {
+    var likedQuote = document.createElement('p');
+    likedQuote.innerHTML = dailyDiv.innerHTML;
+    savedDiv.append(likedQuote);
+    saveDaily.style.display = "none";
+});
 function displayDailyTip() {
     getDailyTip().then(function (data) {
         var authorImage = document.createElement('img');
@@ -69,6 +82,9 @@ function displayDailyTip() {
         authorImage.style.width = "60px";
         dailyDiv.innerHTML = "".concat(data[0].q, " - ").concat(data[0].a);
         dailyDiv.append(authorImage);
+        authorImage.addEventListener('click', function () {
+            console.log(data[0]);
+        });
     });
 }
 ;
@@ -111,8 +127,82 @@ saveButton.addEventListener('click', function () {
     likedQuote.innerHTML = randomDiv.innerHTML;
     savedDiv.append(likedQuote);
     saveButton.style.display = "none";
-    console.log(randomDiv);
 });
 randomButton.addEventListener('click', function () {
     displayRandomTip();
 });
+function getAuthors() {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, authors;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch(authorUrl)];
+                case 1:
+                    response = _a.sent();
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    authors = _a.sent();
+                    return [2 /*return*/, authors];
+            }
+        });
+    });
+}
+function displayAuthors() {
+    getAuthors().then(function (data) {
+        console.log(data);
+        var arrLength = data.length;
+        console.log(arrLength);
+        var _loop_1 = function (i) {
+            var authorInfo = document.createElement('div');
+            authorInfo.className = "authors";
+            var authorImage = document.createElement('img');
+            authorImage.src = data[i].i;
+            authorInfo.innerHTML = data[i].a;
+            authorList.appendChild(authorInfo);
+            authorInfo.appendChild(authorImage);
+            authorImage.addEventListener('click', function () {
+                quotes.innerHTML = "";
+                console.log(data[i]);
+                var cardImage = document.createElement('img');
+                cardImage.src = data[i].i;
+                console.log(cardImage);
+                quotes.appendChild(cardImage);
+                displayOneAuthor();
+                function getLink() {
+                    return __awaiter(this, void 0, void 0, function () {
+                        var response, links;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, fetch(data[i].l + userKey)];
+                                case 1:
+                                    response = _a.sent();
+                                    return [4 /*yield*/, response.json()];
+                                case 2:
+                                    links = _a.sent();
+                                    return [2 /*return*/, links];
+                            }
+                        });
+                    });
+                }
+                ;
+                function displayOneAuthor() {
+                    getLink().then(function (data) {
+                        console.log(data);
+                        var quoteListLength = data.length;
+                        for (var i_1 = 0; i_1 < quoteListLength; i_1++) {
+                            var quoteList = document.createElement('li');
+                            quoteList.innerHTML = data[i_1].q;
+                            quotes.append(quoteList);
+                        }
+                    });
+                }
+            });
+        };
+        for (var i = 0; i < arrLength; i++) {
+            _loop_1(i);
+        }
+        ;
+    });
+}
+;
+displayAuthors();
--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
 
 const urlRandom = 'https://zenquotes.io/api/random/116b01ed99f7230a36d036a31adcc0f796ed6a70'
 const urlDaily = 'https://zenquotes.io/api/today/116b01ed99f7230a36d036a31adcc0f796ed6a70'
+const authorUrl = 'https://zenquotes.io/api/authors/116b01ed99f7230a36d036a31adcc0f796ed6a70'
+const userKey = '116b01ed99f7230a36d036a31adcc0f796ed6a70';
 const randomButton = document.querySelector('.randomize') as HTMLButtonElement;
 const saveButton = document.querySelector('.saveButton') as HTMLButtonElement;
+const saveDaily = document.querySelector('.saveDaily') as HTMLButtonElement;
 const dailyDiv = document.querySelector('.dailyTip') as HTMLElement;
 const randomDiv = document.querySelector('.randomTip') as HTMLElement;
 const buttonDiv = document.querySelector('.buttons') as HTMLElement;
 const savedQuotes = document.querySelector('.savedQuotes') as HTMLElement;
 const savedDiv = document.querySelector('#savedDiv') as HTMLDivElement;
+const authorList = document.querySelector('.authorList') as HTMLElement;
+const body = document.querySelector('#documentFrame') as HTMLElement;
+const quotes = document.querySelector('.quotes') as HTMLElement;
+const authorCard = document.querySelector('.authorCard') as HTMLElement;
 
 
 
@@ -19,6 +26,13 @@ async function getDailyTip() {
     return dataDaily;
 };
 
+saveDaily.addEventListener('click', function () {
+    const likedQuote = document.createElement('p') as HTMLParagraphElement;
+    likedQuote.innerHTML = dailyDiv.innerHTML;
+    savedDiv.append(likedQuote);
+    saveDaily.style.display = "none";
+})
+
 function displayDailyTip() {
     getDailyTip().then((data) => {
         const authorImage = document.createElement('img') as HTMLImageElement;
@@ -27,8 +41,14 @@ function displayDailyTip() {
         authorImage.style.width = "60px"
         dailyDiv.innerHTML = `${data[0].q} - ${data[0].a}`;
         dailyDiv.append(authorImage);
+        authorImage.addEventListener('click', function () {
+            console.log(data[0]);
+        })
     });
 };
+
+
+
 
 window.addEventListener("load", () => {
     displayDailyTip();
@@ -70,3 +90,58 @@ randomButton.addEventListener('click', function() {
     displayRandomTip();
 });
 
+
+async function getAuthors() {
+    const response = await fetch(authorUrl);
+    const authors = await response.json();
+    return authors;
+}
+
+function displayAuthors () {
+    getAuthors().then((data) => {
+        console.log(data);
+        let arrLength = data.length;
+        console.log(arrLength);
+        for (let i = 0; i < arrLength; i++) {
+            const authorInfo = document.createElement('div') as HTMLElement;
+            authorInfo.className = "authors";
+            const authorImage = document.createElement('img') as HTMLImageElement;
+            authorImage.src = data[i].i;
+            authorInfo.innerHTML = data[i].a;
+            authorList.appendChild(authorInfo);
+            authorInfo.appendChild(authorImage);
+
+
+            authorImage.addEventListener('click', function () {
+                quotes.innerHTML = "";
+                console.log(data[i]);
+                const cardImage = document.createElement('img') as HTMLImageElement;
+                cardImage.src = data[i].i;
+                console.log(cardImage);
+                quotes.appendChild(cardImage);
+                displayOneAuthor();
+                async function getLink() {
+                    const response = await fetch(data[i].l + userKey);
+                    const links = await response.json();
+                    return links;
+                };
+
+
+            function displayOneAuthor () {
+                getLink().then((data) => {
+                    console.log(data);
+                    const quoteListLength = data.length;
+                    for (let i = 0; i < quoteListLength; i++) {
+                        const quoteList = document.createElement('li') as HTMLElement;
+                        quoteList.innerHTML = data[i].q;
+                        quotes.append(quoteList);
+                    }
+                })
+            }  
+
+            })
+        };
+    });
+};
+
+displayAuthors();
